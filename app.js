2de8ae46dc2e31262284aac51863355102655b20
Rajat Sharma <71508860+Rajat-Sharma1710@users.noreply.github.com>
2020-10-24 21:27:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname , '/public')));
 app.use(express.urlencoded({ extended:true }))
 app.use("/",main);
-app.use("/short",short);
+app.use("/",short);
 app.listen(Port,()=>{
     console.log(`Listening on port ${Port}`);
 })
